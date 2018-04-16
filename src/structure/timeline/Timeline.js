f81f61c8affab3c2f89700929eaae35eadb894b7
Lysandre DEBUT <lysandre.debut@reseau.eseo.fr>
2018-04-16 19:13:36 +0200

    Added ID to Timeline object.

--- src/structure/timeline/Timeline.js
+++ src/structure/timeline/Timeline.js
@@ -1,10 +1,12 @@
 
 export default class Timeline{
     constructor(initialState){
+        console.log("Creating new Timeline");
         this._blocks = [];
         this._initialState = initialState;
         this._initialState.setID();
         this._states = [this._initialState];
+        this._id = Timeline.getID();
     }
 
     updateState(state){
@@ -22,5 +24,19 @@ export default class Timeline{
 
     getCurrentState(){
         return this._states[this._states.length - 1];
+    }
+
+    getID(){
+        return this._id;
+    }
+
+    static getID(){
+        if(Timeline.id == undefined){
+            Timeline.id = 0;
+        }else{
+            Timeline.id++;
+        }
+
+        return Timeline.id;
     }
 }
